e2afb4ac220ed34afa77dad9f6c1c12e4967c0d4
Lohita-Ifijeh <olohitareii@gmail.com>
2024-09-25 17:52:11 -0400

    fo

--- src/main/java/com/bptn/course/_05_strings/Strings.java
+++ src/main/java/com/bptn/course/_05_strings/Strings.java
@@ -4,5 +4,37 @@ public class Strings {
 
 	public static void main(String[] args) {
 		
+		String s = "Hello world";
+		
+		int stringlength= s.length();
+		
+		System.out.println("The total length of the string is " + stringlength);
+		
+		String sub = s.substring(0, 8);
+		
+		System.out.println("The string with start index is 0 and the 8 is: " + sub);
+		
+		String sub1 = s.substring(4);
+		
+		System.out.println("The sting after index 4 is " + sub1);
+		
+		String lowerCaseString =s.toLowerCase();
+		
+		System.out.println("The lowercase string is " + lowerCaseString);
+		
+		String UpperCase =s.toUpperCase();
+		
+		System.out.println("The Uppercase is " + UpperCase);
+		
+		int index= s.indexOf("world");
+		
+		System.out.println("The index of world is: " + index);
+		
+		char c = s.charAt(0);
+		
+		System.out.println("The character at index 0 is: " + c);
+		
+		
+		
 	}
 }
